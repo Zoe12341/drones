5c2c7891e56fccfc30a7d3ffa1cf712fff80cc62
caseymacgibbon <cmacgibbon@smith.edu>
2025-05-04 19:21:04 -0400

    deleting extra files

--- sci_db.py
+++
@@ -1,7 +0,0 @@
-from scidbpy import connect
-    
-try:
-    connection = connect(host="localhost", port=8080, username="drone", password="Buzzing0verhead")
-    print("Successfully connected to SciDB!")
-except Exception as e:
-    print(f"Error connecting to SciDB: {e}")
